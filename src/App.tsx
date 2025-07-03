41f60f8df6927f6348ba8579076fb6407c624f91
sooyeoniya <tndus1306403@naver.com>
2025-07-03 15:20:58 +0900

    chore: 배포 시 서브 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,11 +15,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/space/:spaceId" element={<SpaceDetail />} />
-          <Route path="/space/:spaceId/sheet/:sheetDate" element={<DailySheet />} />
+          <Route
+            path="/space/:spaceId/sheet/:sheetDate"
+            element={<DailySheet />}
+          />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
